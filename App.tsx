77c2bc4c051e505e0829df07015cbc3d0ead85c5
Mohammed Sithik <mohdsithik0786@gmail.com>
2024-11-10 01:32:09 +0530

    Added status bar color

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import MainStack from './src/navigation/MainStack';
+import {StatusBar} from 'react-native';
+import Colors from './src/constants/Colors';
 
 const App = () => {
   return (
     <NavigationContainer>
+      <StatusBar backgroundColor={Colors.BlackB1B1B} />
       <MainStack />
     </NavigationContainer>
   );
